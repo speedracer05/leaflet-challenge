19e9df357c807e3346d718a19eaf727c9a62af27
speedracer05 <speedracer-05@outlook.com>
2021-09-05 13:13:04 -0700

    add map & legend to css, add get json api call to logic.js

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,5 +1,5 @@
 // Create the tile layer for map
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var baseMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   tileSize: 512
   maxZoom: 18,
@@ -10,7 +10,6 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 // Initialize LayerGroup
 var earthquakes = new L.LayerGroup();
 
-
 // Creating my map object
 // Dublin, CA
 var mymap = L.map("map", {
@@ -19,15 +18,39 @@ var mymap = L.map("map", {
   ],
   zoom: 4,
   layers: [
-    streetmap,
+    baseMap,
     earthquakes]
 });
 
-// Optional  overlay objects to add to the layer control.
-// var overlays = {
-//   "Option 1": layers.COMING_SOON,
-//   "Option 2": layers.EMPTY
-// };
+var baseMaps = {
+  "Street Map": streetmap,
+  "Satellite": satellite
+};
+
+// Add 'baseMap' tile layer to the map
+baseMap.addTo(map);
+
+// Create overlay object
+var overlayMaps = {
+  Earthquakes: earthquakes
+};
+
+// var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: API_KEY
+// });
+
+// var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+//   maxZoom: 18,
+//   id: "satellite-v9",
+//   accessToken: API_KEY
+// });
+
 
 // Create a control for layers, adding overlay layers to it
 L.control.layers(baseMaps, overlayMaps, {
@@ -39,28 +62,11 @@ var info = L.control({
   position: "bottomright"
 });
 
-var baseMaps = {
-  "Street Map": streetmap,
-  "Satellite": satellite
-};
-
-// Add 'baseMaps' tile layer to the map
-baseMaps.addTo(map);
-
-// Create overlay object
-var overlayMaps = {
-  Earthquakes: earthquakes
-};
-
-
 
 // Perform API call to USGS endpoint
 // All Earthquakes in the past week. Retrieved data on 09-04-2021 
-var link =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+var getURL =  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
 // Get link data with d3
-d3.json(link, function(response) {
-  console.log(response.features[0])
-
-  
-}
+d3.json(getURL, function(response) {
+)
