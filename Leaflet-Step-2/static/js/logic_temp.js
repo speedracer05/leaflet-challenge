6e8b4aad8e3d932985b6a645ccc74fb230c4ffa4
speedracer05 <speedracer-05@outlook.com>
2021-09-06 01:19:43 -0700

    debugging. replaced step-2 config.js with step-1, renamed logic.js and copied step-1 logic.js file

--- Leaflet-Step-2/static/js/logic_temp.js
+++ Leaflet-Step-2/static/js/logic_temp.js
@@ -1,5 +1,6 @@
 // Initialize LayerGroup
 var earthquakes = new L.LayerGroup();
+var tec_plates = new L.LayerGroup(); // New layer for tectonics
 
 // Perform API call to USGS endpoint
 // All Earthquakes in the past week. Retrieved data on 09-04-2021 
@@ -61,7 +62,7 @@ function createMap(earthquakes) {
     center: [
       37.7159, -121.9101
     ],
-    zoom: 4,
+    zoom: 3,
     layers: [streetmap, earthquakes]
   });
 
@@ -70,9 +71,20 @@ function createMap(earthquakes) {
     "Satellite": satellite
   };
 
+  // Tectonic boundries data
+  // Source: https://github.com/fraxen/tectonicplates
+  var tec_platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+  d3.json(tec_platesURL, function(tecData){
+    L.geoJSON(tecData,{
+  
+    }).addTo(tec_plates)
+    tec_plates.addTo(myMap)
+  })
+
   // Create overlay object
   var overlayMaps = {
-    Earthquakes: earthquakes
+    Earthquakes: earthquakes,
+    tectonicsplates: tec_plates
   };
 
   // Create a control for layers, adding overlay layers to it
